193e664998e7e177dba2e26af485811ee0800d0e
Cedd Burge <ceddlyburge@gmail.com>
2021-04-09 15:49:41 +0100

    Add hardcoded calculation of optimal game order
    This shows that the azure function integration works, and that the nuget
    package is imported successfully and works

---
+++ OptimalGameOrder/OptimalGameOrder.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using CanoePoloLeagueOrganiser;
+using System.Collections.Generic;
+
+namespace OptimalGameOrder
+{
+    public static class OptimalGameOrder
+    {
+        [FunctionName("OptimalGameOrder")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string name = req.Query["name"];
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            var games = new List<Game> {
+                new Game("Clapham", "Surrey"),
+                new Game("Clapham", "ULU"),
+                new Game("Clapham", "Meridian"),
+                new Game("Blackwater", "Clapham"),
+                new Game("ULU", "Blackwater"),
+                new Game("Surrey", "Castle"),
+                new Game("ULU", "Meridian"),
+                new Game("Letchworth", "ULU"),
+                new Game("Castle", "Blackwater"),
+                new Game("Surrey", "Letchworth"),
+                new Game("Meridian", "Castle"),
+                new Game("Blackwater", "Letchworth"),
+                new Game("Meridian", "Surrey"),
+                new Game("Castle", "Letchworth")
+             };
+
+            var gameOrder = new CanoePoloLeagueOrganiser.OptimalGameOrder(new TenSecondPragmatiser()).OptimiseGameOrder(games);
+            
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(gameOrder);
+        }
+    }
+}
